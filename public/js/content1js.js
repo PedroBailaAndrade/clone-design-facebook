1383678747901b739cbdc1a5c9510a021c1c4cb0
PedroBailaAndrade <52913878+PedroBailaAndrade@users.noreply.github.com>
2021-02-12 00:56:11 +0000

    Text animation

--- public/js/content1js.js
+++ public/js/content1js.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', function () {
 	var text = document.getElementsByClassName("text")[0];
-	console.log(text);
 
-	var p = null;
-	for (var i = 0; i < text.childNodes.length; i++) {
-		var child = text.childNodes[i];
-	    child.style.top = "33px";    
-	}
+	for (i = 0; i < text.children.length; i++) {
+		var child = text.children[i];
+		child.style.animationName = "fadeInBottom";
+		child.style.animationDuration = (1+i)/4 + "s";
+		child.style.animationTimingFunction = "linear"
+		child.style.animationIterationCount = "1";
+    	child.style.animationPlayState = "running";
+    	child.style.animationFillMode = "forwards";
+	} 
 });
